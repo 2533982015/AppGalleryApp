0e0086bf7ce4ca4bef0494d0bc5508211bd0b747
hkathe <101960319+hkathe@users.noreply.github.com>
2022-03-20 02:52:39 -0600

    AppGalleryApp
    Gallery App

--- lib/data/images.dart
+++ lib/data/images.dart
@@ -3,28 +3,28 @@ List<Map<String, dynamic>> data = [
     'category': 'Paisajes',
     'images': [
       {
-        'description': '',
-        'date': '2022/20/03',
+        'description': 'Paisaje bonito para vivir, en Italia',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-1/paisaje-1.jpg'
       },
       {
-        'description': 'this is a description image',
-        'date': '2022/20/03',
+        'description': 'paisaje en la naturaleza',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-1/paisaje-2.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description': 'viendo el mar azul',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-1/paisaje-3.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description': 'El bonito paisaje reflejado en el mar',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-1/paisaje-4.jpg'
       },
       {
-        'description': 'this is a description image',
-        'date': '2022/20/03',
+        'description': 'El paisaje detras de la montaña',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-1/paisaje-5.jpg'
       },
     ]
@@ -33,28 +33,30 @@ List<Map<String, dynamic>> data = [
     'category': 'Flores',
     'images': [
       {
-        'description': 'this is a description image',
-        'date': '2022/20/03',
+        'description': 'En el campo de flores hermosas',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-2/flores1.jpg'
       },
       {
-        'description': 'this is a description image',
-        'date': '2020/09/09',
+        'description':
+            'Las famosas flores de pétalos dobles y colores muy vistosos son llamadas zinnias. Así que si lo que quieres es llenar tu hogar de alegría y buena energía, las zinnias se encargarán de darle vida y color a tu decoración.',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-2/flores2.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description':
+            'Esta flor es reconocida por sus vibrantes colores y por la original forma de la flor ya que parecen pequeñas trompetas a la hora que florecen. Estas hermosas flores también se conocen como campanillas y serán perfectas para decorar tu jardín durante la temporada de calor.',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-2/flores3.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description': 'En el campo',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-2/flores4.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description': 'Vivir en el campo llena',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-2/flores5.jpg'
       },
     ]
@@ -63,28 +65,29 @@ List<Map<String, dynamic>> data = [
     'category': 'Rios',
     'images': [
       {
-        'description': 'this is a description image',
-        'date': '2022/20/03',
+        'description': 'A la orilla del Rio',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-3/rio1.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description': 'En el Rios la vida es mas sabrosa',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-3/rio2.jpg'
       },
       {
-        'description': 'this is a description image',
-        'date': '2020/09/09',
+        'description': 'El rio cerca del parque',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-3/rio3.jpg'
       },
       {
-        'description': 'this is a description image',
-         'date': '2022/20/03',
+        'description':
+            ' Los ríos son desplazamientos de agua que están presentes en la naturaleza, sin ninguna interrupción. Por lo general, un río parte de su naciente y corre en dirección hacia los relieves más bajos hasta culminar en otro curso mayor (por ejemplo: el océano); igualmente, existe la posibilidad de desembocar en un lago.',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-3/rio4.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-3/rio5.jpg'
       },
     ]
@@ -94,27 +97,27 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-4/playa1.jpg'
       },
       {
         'description': 'this is a description image',
-       'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-4/playa2.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-4/playa3.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-4/playa4.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-4/playa5.jpg'
       },
     ]
@@ -124,27 +127,27 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-5/bosque1.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-5/bosque2.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-5/bosque3.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-5/bosque4.jpg'
       },
       {
         'description': 'this is a description image',
-         'date': '2022/20/03',
+        'date': '2022/03/20',
         'url': 'assets/images/categoria-5/bosque5.jpg'
       },
     ]
