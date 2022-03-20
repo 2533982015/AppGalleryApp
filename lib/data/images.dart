7dc9ac3116e221759e38edb5a1d517018e91e988
hkathe <101960319+hkathe@users.noreply.github.com>
2022-03-20 01:15:01 -0600

    AppGalleryApp
    Gallery App

--- lib/data/images.dart
+++ lib/data/images.dart
@@ -3,28 +3,28 @@ List<Map<String, dynamic>> data = [
     'category': 'Paisajes',
     'images': [
       {
-        'description': 'this is a description image',
-        'date': '2020/09/09',
+        'description': '',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-1/paisaje-1.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-1/paisaje-2.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-1/paisaje-3.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-1/paisaje-4.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-1/paisaje-5.jpg'
       },
     ]
@@ -34,7 +34,7 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-2/flores1.jpg'
       },
       {
@@ -44,17 +44,17 @@ List<Map<String, dynamic>> data = [
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-2/flores3.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-2/flores4.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-2/flores5.jpg'
       },
     ]
@@ -64,12 +64,12 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-3/rio1.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-3/rio2.jpg'
       },
       {
@@ -79,12 +79,12 @@ List<Map<String, dynamic>> data = [
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-3/rio4.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-3/rio5.jpg'
       },
     ]
@@ -94,27 +94,27 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-4/playa1.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+       'date': '2022/20/03',
         'url': 'assets/images/categoria-4/playa2.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-4/playa3.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+        'date': '2022/20/03',
         'url': 'assets/images/categoria-4/playa4.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-4/playa5.jpg'
       },
     ]
@@ -124,27 +124,27 @@ List<Map<String, dynamic>> data = [
     'images': [
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-5/bosque1.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-5/bosque2.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-5/bosque3.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-5/bosque4.jpg'
       },
       {
         'description': 'this is a description image',
-        'date': '2020/09/09',
+         'date': '2022/20/03',
         'url': 'assets/images/categoria-5/bosque5.jpg'
       },
     ]
